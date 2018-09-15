b5672be9d89878c70b34d88c37dc722ca2755956
shuistama <shuistama@gmail.com>
2018-09-15 23:23:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
-var http = require('http')
-var dt = require('./myfirstmodule')
 
-var server = http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Hi, Selamat datang di node<b>JS</b>, hari ini tepat '+ dt.myDateTime())
-    res.end();
-});
-server.listen(8080);
-console.log('server running pn http://localhost:8080');
